f6ff4a709baab955e781d4a915cb88e0cafed153
aston <astonbitecode@gmail.com>
2024-11-20 10:31:53 +0200

    Show usage in new threads

--- app/src/main/rust/src/lib.rs
+++ app/src/main/rust/src/lib.rs
@@ -1,5 +1,5 @@
 use android_activity::AndroidApp;
-use j4rs::{InvocationArg, JvmBuilder};
+use j4rs::{InvocationArg, JvmBuilder, Jvm};
 use j4rs::jni_sys::{JavaVM, jobject};
 
 #[no_mangle]
@@ -27,5 +27,24 @@ fn test(app: &AndroidApp) {
       ).unwrap();
       
       let rust_boolean: bool = jvm.to_rust(boolean_instance).unwrap();
-      println!("======================={rust_boolean}");
+      println!("=======================Use in main thread {rust_boolean}");
+
+    let handle = std::thread::spawn(move || {
+        // After the initial Jvm creation, Jvm instances can be created by simply attaching to the thread
+        let other_jvm = Jvm::attach_thread().unwrap();
+        let string_instance = other_jvm.create_instance(
+            "java.lang.String",
+            InvocationArg::empty(),
+        ).unwrap();
+        let boolean_instance = other_jvm.invoke(
+            &string_instance,
+            "isEmpty",
+            InvocationArg::empty(),
+          ).unwrap();
+          
+          let rust_boolean: bool = other_jvm.to_rust(boolean_instance).unwrap();
+          println!("=======================Use in new thread: {rust_boolean}");
+    });
+    handle.join().unwrap();
+    
 }
